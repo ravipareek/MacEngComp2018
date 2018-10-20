7ac4434856ee695d1e638d6e51855f72b55e4612
Pareek Ravi <pareek96@gmail.com>
2018-10-20 19:01:57 -0400

    Text Prediction

---
+++ textpredict.py
@@ -0,0 +1,15 @@
+import autocomplete
+
+autocomplete.load()
+autocomplete.predict('the','b')
+
+print("Hello")
+
+[('blood', 204),
+ ('battle', 185),
+ ('bone', 175),
+ 
+ ('best', 149),
+ ('body', 149),
+]
+
